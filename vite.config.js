4d77fd4f0fe1374cd04ccd026f32dc6de1862812
Dian Eka Setiawan <setiawan.dianeka@gmail.com>
2023-06-17 07:45:25 +0700

    add tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcss from './postcss.config.js'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    }
+  } 
 })
